3c7f7f4c7b3728db78f4e57002dad38b47922a09
vasu.aggarwal <vasu.aggarwal@extramarks.com>
2024-09-03 22:52:36 +0530

    Basic android setup

---
+++ app/src/main/java/com/android/banquetmanager/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.android.banquetmanager.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBookingRepository(firestore: FirebaseFirestore): BookingRepository {
+        return BookingRepository(firestore)
+    }
+}
