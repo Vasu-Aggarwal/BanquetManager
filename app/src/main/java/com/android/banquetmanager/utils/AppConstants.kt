61a7def339a808e476b348bef394b72702dbb28e
vasu.aggarwal <vasu.aggarwal@extramarks.com>
2024-09-26 16:58:25 +0530

    Created constants for user management

--- app/src/main/java/com/android/banquetmanager/utils/AppConstants.kt
+++ app/src/main/java/com/android/banquetmanager/utils/AppConstants.kt
@@ -4,4 +4,11 @@ object AppConstants {
     const val NORMAL_TEXT = 12
     const val SUBHEADING_TEXT = 14
     const val HEADING_TEXT = 16
+    const val CAN_ADD_USERS = "canAddUsers"
+    const val CAN_CHECK_PRICES = "canCheckPrices"
+    const val CAN_READ_DATA = "canReadData"
+    const val CAN_WRITE_DATA = "canWriteData"
+    const val ROLE_NORMAL = "normal"  //Can read data
+    const val ROLE_ADMIN = "admin"    //Can read, write, check balances
+    const val ROLE_SUPER_ADMIN = "superAdmin" //Can read, write, check balances, Add other users and set permission
 }
